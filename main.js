0b7c87a1c650e338c9fe743ce2a6283e5e07d2fc
Jack Browne <jackbrowne00@gmail.com>
2021-04-23 23:08:42 +0100

    task 5 game loop complete

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 let gamesPlayed = 0;
+let wingames = 0;
+let drawgames = 0;
+let losegames = 0;
+
+let anotherPlay = true;
 
 function playGame() {
-
-gamesPlayed++;
-console.log (gamesPlayed);
 
 let player1 = "";
 let player2 =  "rock";
 
 player1 = prompt ("Please choose rock, paper or scissors");
-if (prompt = false){
-    anotherPlay = false;
-};
-
 
 while ((player1 !== ("rock")) && (player1 !== ("paper")) && (player1 !== ("scissors"))) {
     player1 = prompt ("Invalid choice. Please choose rock, paper or scissors.")
@@ -35,52 +33,62 @@ console.log (player2);
 
 function getWinner(player1, player2) {
     if (player1 === "rock" && player2 === "rock"){
+        drawgames++;
         return `0`;
     }
     if (player1 === "paper" && player2 === "rock"){
+        wingames++;
         return `1`;
     }
     if (player1 === "scissors" && player2 === "rock"){
+        losegames++;
         return `-1`;
     }
     if (player1 === "rock" && player2 === "paper"){
+        losegames++;
         return `-1`;
     }
     if (player1 === "paper" && player2 === "paper"){
+        drawgames++;
         return `0`;
     }
     if (player1 === "scissors" && player2 === "paper"){
+        wingames++;
         return `1`;
     }
     if (player1 === "rock" && player2 === "scissors"){
+        wingames++;
         return `1`;
     }
     if (player1 === "paper" && player2 === "scissors"){
+        losegames++;
         return `-1`;
     }
     if (player1 === "scissors" && player2 === "scissors"){
+        drawgames++;
         return `0`;
     }
 };
+
+gamesPlayed++;
+console.log (gamesPlayed);
 
 let outcome = getWinner(player1, player2);
 
 let wordoutcome = ("");
 
 if (outcome == 1){
-    wordoutcome = "WINNER!";
+    wordoutcome = "Won";
 } else if (outcome == 0) {
-    wordoutcome = "DRAW!";
+    wordoutcome = "Drew";
 } else if (outcome == -1){
-    wordoutcome = "LOSER!";
+    wordoutcome = "Lost";
 }
 
-alert(`You chose ${player1}, the computer chose ${player2}. The outcome of this game is ${wordoutcome}`)
+alert(`You chose ${player1}, the computer chose ${player2}. You ${wordoutcome} this round. Out of the ${gamesPlayed} games you have played, you have won ${wingames} games, drew ${drawgames} and lost ${losegames}.`)
 
 anotherPlay =  confirm("Would you like to play again?");
 }
-
-let anotherPlay = true;
 
 while (anotherPlay === true){
     playGame()
