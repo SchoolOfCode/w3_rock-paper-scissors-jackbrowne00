e0398e5fd07aeacabac4e04c02b7e438ee120d5b
Jack Browne <jackbrowne00@gmail.com>
2021-04-23 21:46:26 +0100

    task 5 completed, game loop + confirm

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+let gamesPlayed = 0;
+
+function playGame() {
+
+gamesPlayed++;
+console.log (gamesPlayed);
+
 let player1 = "";
 let player2 =  "rock";
 
 player1 = prompt ("Please choose rock, paper or scissors");
+if (prompt = false){
+    anotherPlay = false;
+};
+
 
 while ((player1 !== ("rock")) && (player1 !== ("paper")) && (player1 !== ("scissors"))) {
     player1 = prompt ("Invalid choice. Please choose rock, paper or scissors.")
+    if (prompt = false){
+        anotherPlay = false;
+    }
 };
 
 console.log(player1);
@@ -47,10 +61,27 @@ function getWinner(player1, player2) {
     if (player1 === "scissors" && player2 === "scissors"){
         return `0`;
     }
+};
+
+let outcome = getWinner(player1, player2);
+
+let wordoutcome = ("");
+
+if (outcome == 1){
+    wordoutcome = "WINNER!";
+} else if (outcome == 0) {
+    wordoutcome = "DRAW!";
+} else if (outcome == -1){
+    wordoutcome = "LOSER!";
 }
 
-let outcome = getWinner(player1, player2)
+alert(`You chose ${player1}, the computer chose ${player2}. The outcome of this game is ${wordoutcome}`)
 
-console.log(outcome)
+anotherPlay =  confirm("Would you like to play again?");
+}
 
-alert(outcome);
+let anotherPlay = true;
+
+while (anotherPlay === true){
+    playGame()
+}
